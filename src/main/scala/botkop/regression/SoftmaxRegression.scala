d956b49faa0629f66413e4defcea3597ef7f495f
Koen Dejonghe <koen.dejonghe@vdab.be>
2019-09-04 08:30:05 +0200

    smr

--- src/main/scala/botkop/regression/SoftmaxRegression.scala
+++ src/main/scala/botkop/regression/SoftmaxRegression.scala
@@ -2,14 +2,22 @@ package botkop.regression
 
 import botkop.{numsca => ns}
 import botkop.data.FashionMnistDataLoader
+import com.typesafe.scalalogging.LazyLogging
 import scorch.autograd.Variable
+import scorch.data.loader.DataLoader
 
-object SoftmaxRegression extends App {
+object SoftmaxRegression extends App with LazyLogging {
 
   val batchSize = 256
 
+  logger.info("reading training data set")
   val trainDl = new FashionMnistDataLoader("train", batchSize)
-  println("data set read")
+  logger.info("training data set read")
+
+  logger.info("reading test data set")
+  val testDl = new FashionMnistDataLoader("test", batchSize)
+  testDl.normalize(trainDl.meanImage)
+  logger.info("test data set read")
 
   val numFeatures = trainDl.numFeatures
   val numClasses = 10
@@ -27,19 +35,32 @@ object SoftmaxRegression extends App {
       p.grad.data := 0.0
     }
 
-  val numEpochs = 5
-  val lr = 0.1
+  def evaluate(dl: DataLoader, net: Variable => Variable): (Double, Double) = {
+    val (l, a) = dl.foldLeft(0.0, 0.0) {
+      case ((lossAcc, accuracyAcc), (x, y)) =>
+        val output = net(x)
+        val guessed = ns.argmax(output.data, axis = 1)
+        val accuracy = ns.sum(guessed == y.data)
+        val cost = loss(output, y).data.squeeze()
+        (lossAcc + cost, accuracyAcc + accuracy)
+    }
+    (l / dl.numBatches, a / dl.numSamples)
+  }
 
-  (0 until numEpochs).foreach { epoch =>
+  val numEpochs = 15
+  val lr = 0.3
+
+  (0 until numEpochs) foreach { epoch =>
+    logger.info(s"starting epoch $epoch")
     trainDl.foreach {
       case (x, y) =>
         val yh = net(x)
         val l = loss(yh, y)
-        println(l)
         l.backward()
         sgd(Seq(w, b), lr, batchSize) // update parameters using their gradient
     }
-
+    val (l, a) = evaluate(testDl, net)
+    logger.info(s"epoch $epoch\tloss: $l\taccuracy: $a")
   }
 
 }
