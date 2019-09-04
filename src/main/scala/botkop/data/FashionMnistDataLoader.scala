ec354cb7c299951c99c94fcd86781b214818b00f
Koen Dejonghe <koen.dejonghe@vdab.be>
2019-09-04 14:20:42 +0200

    std ci

--- src/main/scala/botkop/data/FashionMnistDataLoader.scala
+++ src/main/scala/botkop/data/FashionMnistDataLoader.scala
@@ -1,7 +1,7 @@
 package botkop.data
 
+import botkop.numsca.Tensor
 import botkop.{numsca => ns}
-import ns.Tensor
 import com.typesafe.scalalogging.LazyLogging
 import scorch.autograd.Variable
 import scorch.data.loader.DataLoader
@@ -24,15 +24,17 @@ class FashionMnistDataLoader(val mode: String,
     case "test"  => "data/fashionmnist/fashion-mnist_test.csv"
   }
 
-  def getLines: List[String] = {
+  val lines: List[String] = {
+    logger.info(s"starting to read $file")
     val src = Source.fromFile(file)
     val lines = src.getLines().toList
     src.close()
+    logger.info(s"reading $file: done")
     Random.shuffle(lines.tail) // skip header
   }
 
   val numFeatures = 784
-  val numEntries: Int = getLines.length
+  val numEntries: Int = lines.length
 
   override val numSamples: Int = take match {
     case Some(n) => math.min(n, numEntries)
@@ -43,25 +45,46 @@ class FashionMnistDataLoader(val mode: String,
     (numSamples / miniBatchSize) +
       (if (numSamples % miniBatchSize == 0) 0 else 1)
 
-  // this should fit in memory
-  val data: Seq[(Variable, Variable)] = getLines
+//  val data: Seq[(Variable, Variable)] = lines
+//    .take(take.getOrElse(numSamples))
+//    .sliding(miniBatchSize, miniBatchSize)
+//    .map { lines =>
+//      val (xData, yData) = lines
+//        .foldLeft(List.empty[Float], List.empty[Float]) {
+//          case ((xs, ys), line) =>
+//            val tokens = line.split(",")
+//            val (y, x) =
+//              (tokens.head.toFloat, tokens.tail.map(_.toFloat / 255).toList)
+//            (x ::: xs, y :: ys)
+//        }
+//      val x = Variable(Tensor(xData.toArray).reshape(yData.length, numFeatures))
+//      val y = Variable(Tensor(yData.toArray).reshape(yData.length, 1))
+//      (x, y)
+//    }
+//    .toSeq
+
+  val data: Seq[(Variable, Variable)] = lines
     .take(take.getOrElse(numSamples))
     .sliding(miniBatchSize, miniBatchSize)
-    .toSeq
     .map { lines =>
-      val (xData, yData) = lines
-        .foldLeft(List.empty[Float], List.empty[Float]) {
-          case ((xs, ys), line) =>
-            val tokens = line.split(",")
-            val (y, x) =
-              (tokens.head.toFloat, tokens.tail.map(_.toFloat / 255).toList)
-            (x ::: xs, y :: ys)
-        }
+      val batchSize = lines.length
 
-      val x = Variable(Tensor(xData.toArray).reshape(yData.length, numFeatures))
-      val y = Variable(Tensor(yData.toArray).reshape(yData.length, 1))
+      val xs = Array.fill[Float](numFeatures * batchSize)(elem = 0)
+      val ys = Array.fill[Float](batchSize)(elem = 0)
+
+      lines.zipWithIndex.foreach { case (line, lnr) =>
+        val tokens = line.split(",")
+        ys(lnr) = tokens.head.toFloat
+        tokens.tail.zipWithIndex.foreach { case (sx, i) =>
+          xs(lnr * numFeatures + i) = sx.toFloat / 255
+        }
+      }
+
+      val x = Variable(Tensor(xs).reshape(batchSize, numFeatures))
+      val y = Variable(Tensor(ys).reshape(batchSize, 1))
       (x, y)
     }
+    .toSeq
 
   lazy val meanImage: Tensor = {
     val m = ns.zeros(1, numFeatures)
--- src/main/scala/botkop/regression/SoftmaxRegression.scala
+++ src/main/scala/botkop/regression/SoftmaxRegression.scala
@@ -31,24 +31,25 @@ object SoftmaxRegression extends App with LazyLogging {
 
   def sgd(params: Seq[Variable], lr: Double, batchSize: Int): Unit =
     params.foreach { p =>
-      p.data -= lr * p.grad.data / batchSize
+      p.data -= lr * p.grad.data
       p.grad.data := 0.0
     }
 
   def evaluate(dl: DataLoader, net: Variable => Variable): (Double, Double) = {
-    val (l, a) = dl.foldLeft(0.0, 0.0) {
-      case ((lossAcc, accuracyAcc), (x, y)) =>
-        val output = net(x)
-        val guessed = ns.argmax(output.data, axis = 1)
-        val accuracy = ns.sum(guessed == y.data)
-        val cost = loss(output, y).data.squeeze()
-        (lossAcc + cost, accuracyAcc + accuracy)
-    }
+    val (l, a) =
+      dl.foldLeft(0.0, 0.0) {
+        case ((lossAcc, accuracyAcc), (x, y)) =>
+          val output = net(x)
+          val guessed = ns.argmax(output.data, axis = 1)
+          val accuracy = ns.sum(guessed == y.data)
+          val cost = loss(output, y).data.squeeze()
+          (lossAcc + cost, accuracyAcc + accuracy)
+      }
     (l / dl.numBatches, a / dl.numSamples)
   }
 
-  val numEpochs = 15
-  val lr = 0.3
+  val numEpochs = 5
+  val lr = 0.5
 
   (0 until numEpochs) foreach { epoch =>
     logger.info(s"starting epoch $epoch")
